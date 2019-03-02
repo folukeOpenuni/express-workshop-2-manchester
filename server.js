1f2c01e52e481494108b23d838594ba12b4a1021
folukeOpenuni <folukeagbede@googlemail.com>
2019-03-02 15:09:43 +0000

    edited vertion of the original

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
 const express = require("express");
 
-const app = express();
+const handlebars = require("express-handlebars"); // load the package
+
+const app = express(); // already-existing app initialisation
+
+app.engine("handlebars", handlebars()); // initialise the handlebars engine
+app.set("view engine", "handlebars"); // specify engine for page rendering```
 
 app.use(express.static("public"));
 
+var options = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric"
+};
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+  //res.sendFile(__dirname + "/views/index.html");
+  // res.render("index");
+  res.render("index", {
+    firstName: "Foluke",
+    lastName: "Agbede",
+    //date: Date(Date.now())
+    //date: `${new Date().getMonth() + 1} / ${new Date().getFullYear()}`
+    date: new Date().toLocaleDateString("en-GB", options)
+  });
 });
 
 app.get("/my-cv", (req, res) => {
-  res.sendFile(__dirname + "/views/my-cv.html");
+  //res.sendFile(__dirname + "/views/my-cv.html");
+  res.render(__dirname + "/views/my-cv.handlebars");
 });
 
 const SERVER_PORT = process.env.PORT || 3000;
